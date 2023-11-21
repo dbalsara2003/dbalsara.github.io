d1c0fd40d68c1f56b0526502f971e79609dc5ad7
dbalsara2003 <dbalsara2003@gmail.com>
2023-11-21 14:32:56 -0800

    fixing dark theme switcher

--- JS/theme-switch.js
+++ JS/theme-switch.js
@@ -2,19 +2,33 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Check the initial theme preference
     if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('bg-dark', 'text-white');
+        setDarkTheme();
         document.getElementById('theme-toggle').checked = true;
+    } else {
+        setLightTheme();
     }
 
     // Toggle the theme on checkbox change
     document.getElementById('theme-toggle').addEventListener('change', function () {
         if (this.checked) {
-            document.body.classList.add('bg-dark', 'text-white');
+            setDarkTheme();
             localStorage.setItem('theme', 'dark');
         } else {
-            document.body.classList.remove('bg-dark', 'text-white');
+            setLightTheme();
             localStorage.setItem('theme', 'light');
         }
     });
+
+    function setDarkTheme() {
+        document.getElementById('light-theme').disabled = true;
+        document.getElementById('dark-theme').disabled = false;
+        document.getElementById('theme-icon').innerHTML = '<i class="fas fa-moon"></i>';
+    }
+
+    function setLightTheme() {
+        document.getElementById('light-theme').disabled = false;
+        document.getElementById('dark-theme').disabled = true;
+        document.getElementById('theme-icon').innerHTML = '<i class="fas fa-sun"></i>';
+    }
 });
 
